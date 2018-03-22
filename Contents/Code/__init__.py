01f18e97f1f186006fb1aeb28c3e1fd5193ae9de
JayXon <jayxon@gmail.com>
2018-03-21 23:21:40 -0700

    Bug fixes

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,21 +2,17 @@ import time
 
 API_BASE_URL = "https://api.douban.com/v2/movie/"
 DOUBAN_MOVIE_SEARCH = API_BASE_URL + 'search?q=%s'
-DOUBAN_MOVIE_SUBJECT = API_BASE_URL + 'subject/%s/'
-DOUBAN_MOVIE_IMDB_QUERY = API_BASE_URL + 'imdb/%s/'
-DOUBAN_MOVIE_BASE = 'http://movie.douban.com/subject/%s/'
+DOUBAN_MOVIE_SUBJECT = API_BASE_URL + 'subject/%s'
+DOUBAN_MOVIE_IMDB_QUERY = API_BASE_URL + 'imdb/%s'
 REQUEST_RETRY_LIMIT = 3
 
 RE_IMDB_ID = Regex('^tt\d+$')
 RE_DOUBAN_ID = Regex('\d+$')
 
-################################################################################################
-
 
 def Start():
     HTTP.CacheTime = CACHE_1WEEK
 
-################################################################################################
 
 class DBMAgent(Agent.Movies):
     name = 'Douban Movie Database'
@@ -52,7 +48,7 @@ class DBMAgent(Agent.Movies):
             return
 
         # automatic search
-        search_url = DOUBAN_MOVIE_SEARCH % media.name
+        search_url = DOUBAN_MOVIE_SEARCH % String.Quote(media.name)
         dbm_dict = self.get_json(url=search_url)
         if dbm_dict and 'subjects' in dbm_dict:
 
@@ -94,35 +90,32 @@ class DBMAgent(Agent.Movies):
                     else:
                         score = score - (5 * year_diff)
 
-                if score <= 0:
-                    continue
-                else:
-                    imdb_id = self.get_imdb(movie['id'])
-                    if imdb_id:
-                        results.Append(MetadataSearchResult(
-                            id = imdb_id,
-                            name = movie['title'],
-                            year = release_year,
-                            score = score,
-                            lang = lang
-                        ))
+                if score < 0:
+                    score = 0
+
+                results.Append(MetadataSearchResult(
+                    id = movie['id'],
+                    name = movie['title'],
+                    year = release_year,
+                    score = score,
+                    lang = lang
+                ))
 
 
     def update(self, metadata, media, lang):
 
         proxy = Proxy.Preview
 
-        dbm_imdb = self.get_json(url=DOUBAN_MOVIE_IMDB_QUERY % metadata.id)
-
-        # Try to get douban id
-        if not dbm_imdb or not dbm_imdb['id']:
+        if RE_IMDB_ID.search(metadata.id):
+            url=DOUBAN_MOVIE_IMDB_QUERY % metadata.id
+        elif RE_DOUBAN_ID.search(metadata.id):
+            url=DOUBAN_MOVIE_SUBJECT % metadata.id
+        else:
+            Log('Cannot find douban id or imdb id')
             return
-        m = RE_DOUBAN_ID.search(dbm_imdb['id'])
-        if not m:
-            Log('Cannot find douban id from imdb query')
-            return
-        dbm_dict = self.get_json(url=DOUBAN_MOVIE_SUBJECT % m.group(0))
+        dbm_dict = self.get_json(url)
         if not dbm_dict:
+            Log('Failed to get data')
             return
 
         # Rating
@@ -146,56 +139,34 @@ class DBMAgent(Agent.Movies):
         for genre in dbm_dict['genres']:
             metadata.genres.add(genre.strip())
 
-        # Tagline
-        metadata.tagline = ' '.join([ i['name'] for i in dbm_imdb['tags'] ])
-
         # Directors
         metadata.directors.clear()
-        for member in dbm_imdb['attrs']['director']:
-            metadata.directors.add(member)
+        for director in dbm_dict['directors']:
+            d = metadata.directors.new()
+            d.name = director['name']
+            if director['avatars']:
+                d.photo = director['avatars']['large']
 
         # Writers
         metadata.writers.clear()
-        for member in dbm_imdb['attrs']['writer']:
-            metadata.writers.add(member)
 
         # Casts
         metadata.roles.clear()
-        for member in dbm_imdb['attrs']['cast']:
+        for cast in dbm_dict['casts']:
             role = metadata.roles.new()
-            role.actor = member
+            role.name = cast['name']
+            if cast['avatars']:
+                role.photo = cast['avatars']['large']
 
+        # Posters
         if len(metadata.posters.keys()) == 0:
-
             poster_url = dbm_dict['images']['large']
             thumb_url = dbm_dict['images']['small']
             metadata.posters[poster_url] = proxy(HTTP.Request(thumb_url), sort_order=1)
 
-    def get_imdb(self, db_id):
-        url = DOUBAN_MOVIE_BASE % db_id
-        for t in reversed(range(REQUEST_RETRY_LIMIT)):
-            try:
-                result = HTML.ElementFromURL(url)
-            except:
-                Log('Error fetching HTML from Douban Site, will try %s more time(s) before giving up.', str(t))
-                time.sleep(5)
-                continue
-            else:
-                break
-
-        if not result:
-            return None
-
-        for el in result.xpath('//div[@id="info"]/a[@rel="nofollow"]/text()'):
-            m = RE_IMDB_ID.search(el)
-            if m:
-                return m.group(0)
-
-        return None
-
 
     def get_json(self, url, cache_time=CACHE_1HOUR * 3):
-    # try n times waiting 5 seconds in between if something goes wrong
+        # try n times waiting 5 seconds in between if something goes wrong
         result = None
         for t in reversed(range(REQUEST_RETRY_LIMIT)):
             try:
@@ -208,7 +179,5 @@ class DBMAgent(Agent.Movies):
             if isinstance(result, dict):
                 return result
 
-        Log('Error fetching JSON from The Movie Database.')
+        Log('Error fetching JSON from Douban.')
         return None
-
-
